9d49a2169b7a90cd77171436c0e824834eb5256e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 16:54:37 +0800

    Updated test.

--- arlev-test.js
+++ arlev-test.js
@@ -2,6 +2,6 @@
 const assert = require( "assert" );
 const arlev = require( "./arlev.js" );
 
-assert.equal( arlev( [ [ [ ] ] , [ [ [ ] ] ] ] ), 4, "should be equal" );
+assert.equal( arlev( [ [ [ ] ] , [ [ [ ] ] ] ] ), 4, "should be equal to 4" );
 
 console.log( "ok" );
